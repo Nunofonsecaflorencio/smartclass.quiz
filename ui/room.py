cd1da841b27aeb3ee476e8209a051c138f271019
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-10-05 18:28:21 +0200

    feat: gameScreen.py

--- ui/room.py
+++ ui/room.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 import threading
 
+import json
+
 
 class RoomScreen:
     def __init__(self, server) -> None:
@@ -21,7 +23,19 @@ class RoomScreen:
         threading.Thread(target=self.server.start, daemon=True).start()
 
     def update_topic_and_ranking(self, quiz_data):
-        pass
+        self.window['-TOPIC-'].update(quiz_data.get('topic', ''))
+        self.window['-TABLE-'].update([[]])
+
+    def open_file():
+        file_path = sg.popup_get_file(
+            'Selecione o quiz', no_window=True, file_types=(('JSON Files', '*.json'),))
+
+        if file_path:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            return data
+
+        return None
 
     def show(self):
         while True:
@@ -30,4 +44,15 @@ class RoomScreen:
             if event == sg.WIN_CLOSED:
                 self.server.close()
                 break
+
+            if event == '-START-':
+                sg.popup('Jogo Iniciado', 'Tempo: {} minutos'.format(
+                    int(values['-TIMER-'])))
+                # TODO
+
+            if event == 'Criar Sala':
+                quiz_data = self.open_file()
+
+                if quiz_data:
+                    self.update_topic_and_ranking(quiz_data)
         self.window.close()
